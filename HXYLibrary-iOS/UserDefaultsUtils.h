c279ed7a8aa922ba68c6b41362474940824af6dd
SharonHu1990 <sharonhu0813@yeah.net>
2015-09-06 16:58:41 +0800

    Revert "1.添加pch文件"
    This reverts commit 4ca490ea288b59b05304dbf9ed314a74b31d7a45.

--- HXYLibrary-iOS/UserDefaultsUtils.h
+++ HXYLibrary-iOS/UserDefaultsUtils.h
@@ -1,7 +1,7 @@
 //
 //  UserDefaultsUtils.h
 //  HXYLibrary-iOS
-//  键值对操作
+//
 //  Created by 胡晓阳 on 15/8/31.
 //  Copyright (c) 2015年 HXY. All rights reserved.
 //
@@ -9,16 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface UserDefaultsUtils : NSObject
-
-
-+(void)saveValue:(id)value forKey:(NSString *)key;
-
-+(id)valueForKey:(NSString *)key;
-
-+(BOOL)boolValueWithKey:(NSString *)key;
-
-+(void)saveBoolValue:(BOOL)value withKey:(NSString *)key;
-
-+(void)print;
 
 @end
